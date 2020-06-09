369f3c87028e20faf9472ff6caad0b7ff4e54800
usac201612203 <2903905670101@ingenieria.usac.edu.gt>
2020-06-09 12:34:28 -0600

    corto1

---
+++ Corto1/collatz.py
@@ -0,0 +1,23 @@
+#SERGIO ESTUARDO RUANO NAJARRO 
+#201612203
+
+
+
+
+def collatz(num):
+    while(num != 1):
+        if(num %2> 0):
+            num = 3*num +1
+            yield num
+        else:
+            num = num/2
+            yield num
+
+            
+   
+n=203
+for i in range(2,n):
+    print(list(collatz(i)))
+  
+
+
