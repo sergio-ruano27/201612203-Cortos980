3aad12463fb1b83f693bfe3f59b2eb1977d5953f
Ottoniel Sierra <ottonielsierrama@gmail.com>
2020-06-10 12:06:21 -0600

    corto calificado

--- corto1/collatz.py
+++ corto1/collatz.py
@@ -5,6 +5,8 @@
 
 
 def collatz(num):
+    #agregar para obtener secuencia completa
+    #yield num
     while(num != 1):
         if(num %2> 0):
             num = 3*num +1
@@ -19,5 +21,11 @@ n=203
 for i in range(2,n):
     print(list(collatz(i)))
   
-
+#Funcionamiento:        30/40 no guarda las secuencias en un archivo
+#Uso de Funciones       20/20
+#Manejo de archivos     0/10
+#Manejo de Listas       10/10
+#Uso de git             20/20
+#*****************************
+#Total                  80/100
 
